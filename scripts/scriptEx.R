7c89b8f84f8dc284aca2e5c1dc984d86c9f3efff
KevCaz <kevin.cazelles@gmail.com>
2016-05-22 00:49:10 -0400

    exo2 part1

---
+++ scripts/scriptEx.R
@@ -0,0 +1,81 @@
+#######
+##
+##
+##
+##
+##
+##
+##
+setwd("~/Github/formations/QCBSgraphR")
+##
+load("data/word_eng.Rdata")
+##
+countbin <- function(pos, sz, bin=15){
+  seqy <- seq(0, sz, len=bin+1)
+  bin <- which(floor(seqy-floor((pos-1)))==0)
+  val1 <- bin[min(out)
+  return(out)
+  seq(bd1,bd2)
+}
+countbin(2, sz=12, bin=15)
+
+##
+letter_tot <- unlist(strsplit(word_eng,""))
+letter_freq <- table(letter_tot)/length(letter_tot)*100
+# plot(letter_freq, type="h")
+##
+mycount <- function(x,pattern="a"){
+  vec <- unlist(strsplit(x,""))
+  val <- grep(pattern, vec)
+  if (length(val)==0) out <- 0 else {
+
+  }
+  return(out)
+}
+##
+datlet <- data.frame(matrix(0,15,26))
+colnames(datlet) <- letters[1:26]
+
+for (i in 1:26){
+  cat(paste0("\r letter:  ", letters[i]))
+  cool0 <- unlist(lapply(as.list(word_eng), FUN=mycount, letters[i]))
+  cool <- cool0[cool0!=0]
+  for(j in 1:length(cool)){
+    x <- cool[j]
+    datlet[floor(x), i] <- datlet[floor(x), i] + (1-(x-floor(x)))
+    if ((x-floor(x))>0) datlet[floor(x)+1,i] <- datlet[floor(x)+1,i]+(x-floor(x))
+  }
+}
+cat("\n")
+
+
+## Figure
+pdf("~/Desktop/cool.pdf", width=6, height=12)
+
+mypal <- colorRampPalette(c("#ffffd1","#981729"))(8)
+mybin <- c(0,0.1,0.5,1,2,3,5,9)
+
+fallInto <- function (x) max(which(x > mybin))
+categ <- sapply(letter_freq, fallInto)
+
+lay_mat <- matrix(1:26,13)
+lay_mat <- rbind(27,lay_mat,28)
+layout(lay_mat)
+
+par(mar=c(1,1.8,0,0), las=1)
+for (i in 1:26){
+  vec <- datlet[,i]/sum(datlet[,i])
+  plot(vec, type = 'l', axes=FALSE, ann=FALSE)
+  polygon(c(1,1:15,15),c(0,vec,0), border=NA, col=mypal[categ[i]])
+  mtext(side=2, letters[i], cex=1.2)
+}
+## Title
+plot(0,0, ann=FALSE, axes=FALSE, type="n")
+text(0,.35,c("Distribution of English Letters toward"),cex=2)
+text(0,-.45,c("beggining, middle, and end of words"),cex=2)
+## Legend
+par(mar=c(3,8,1.5,8), mgp=c(3,0.4,0))
+image(matrix(1:8,ncol=1), col=mypal, axes=FALSE, ann=FALSE)
+axis(1, at=seq(-1/14,1+1/14,len=9),labels=paste0(c(mybin,13), "%"), lwd=0)
+
+dev.off()
